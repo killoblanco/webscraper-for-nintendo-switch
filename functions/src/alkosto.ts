bc2ab01927cead5f1ce7e6be63616a9e645d779f
Kamilo Vasquez <killo.blanco@gmail.com>
2021-11-09 00:46:34 -0500

    Create alkosto.ts

---
+++ functions/src/alkosto.ts
@@ -0,0 +1,31 @@
+import {CheckStockProps} from "./types";
+
+/**
+ * Check stock
+ * @param browser
+ * @param url
+ */
+async function checkStock({browser, url}: CheckStockProps) {
+  const page = await browser.newPage();
+  await page.goto(url);
+  const items = await page.$$("h2 a.js-product-click-datalayer");
+  let hasStock = false;
+  let shopUrl = "";
+  for (const item of items) {
+    const text = await item.textContent();
+    hasStock = text ? text.toLocaleLowerCase().includes("oled") : false;
+    shopUrl = hasStock ?
+        (await item.getProperty("href") as unknown as string) :
+        "";
+  }
+  await page.close();
+  return {hasStock, shopUrl};
+}
+
+const alkosto = {
+  store: "Alkosto",
+  url: "https://www.alkosto.com/videojuegos/consolas/consolas-nintendo/c/BI_0390_ALKOS?sort=price-desc",
+  checkStock,
+};
+
+export default alkosto;
